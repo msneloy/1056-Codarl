a599aa288b0aa0c6e41a30e3d675bdbdaa1de1c0
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-02-27 11:34:53 +0800

    I2C init

--- 1056-Codarl.ino
+++ 1056-Codarl.ino
@@ -1,116 +1,53 @@
+#include <LiquidCrystal_I2C.h> //https://codeload.github.com/johnrickman/LiquidCrystal_I2C/zip/master
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "WAC.h"
+const char *ssid = SSID;
+const char *password = PW;
+//Create WAC.h to store creds
+//#define SSID "Wireless Access Point"
+//#define PW "Password"
+const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 
-
-int HOURS_PINS[5] = {2, 0, 4, 5, 16};
-//int HOURS_PINS[5] = {16, 5, 4, 0, 2};
-int MINUTES_PINS[6] = {1, 3, 15, 13, 12, 14};
-//int MINUTES_PINS[6] = {14, 12, 13, 15, 3, 1};
-
-int HOURS = 0;
-int MINUTES = 0;
-
+int sensorValue = 0;  // value read from the pot
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 28800, 60000);
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
-
-void connectToWiFi(){
-
-char* ssid = "";  //wifi ssid
-char* password = "";   //wifi password
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
+void setup(){
+  Serial.begin(115200);
+  lcd.init();                   
+  lcd.backlight();
+  lcd.print("System Armed");
+  lcd.clear();     
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    lcd.setCursor(0, 0);
+    lcd.print("Connecting to");
+    lcd.setCursor(0, 1); 
+    lcd.print(ssid); 
+    Serial.print(".");
+  }
+  timeClient.begin();
+  lcd.clear();
 }
 
-
-void updateTime(int *hours, int *minutes){
-
-    
-    String formattedTime;
-    String stringHours;
-    String stringMinutes;
-
-    //get timestamp
-    while(!timeClient.update()) {
-        timeClient.forceUpdate();
-    }
-    formattedTime = timeClient.getFormattedTime();
-
-    //convert string timestamp to int
-    stringHours = formattedTime.substring(0, 2);
-    stringMinutes = formattedTime.substring(3, 5);
-
-    *hours = atoi(stringHours.c_str());
-    *minutes = atoi(stringMinutes.c_str());
-}
-
-void setup() {
-
-  //start serial communication
-  Serial.begin(9600);
-
-    //connect to wifi 
-    connectToWiFi();
-
-    timeClient.begin();
-  
-    
-  //set led pin's as output
-  for(int i = 0; i < 5; i++)
-      pinMode(HOURS_PINS[i], OUTPUT);
-  for(int i = 0; i < 6; i++)
-      pinMode(MINUTES_PINS[i], OUTPUT); 
-  
-    
-
-}
-
-void loop() {
-
-    Serial.println("\n------------------------------------------\n");
-
-    //update hours & seconds    
-    updateTime(&HOURS, &MINUTES);
-    
-    Serial.print("Hours : ");
-    Serial.println(HOURS);
-
-    Serial.print("Binary : ");
-    //convert hours to binary
-    for(int i = 0; i < 5; i++){
-        digitalWrite(HOURS_PINS[i], HOURS % 2);
-        Serial.print(HOURS % 2);
-        HOURS /= 2;
-    }
-    Serial.println();
-    Serial.println();
-
-    Serial.print("Minutes : ");
-    Serial.println(MINUTES);
-    Serial.print("Binary : ");
-    //convert minutes to binary
-    for(int i = 0; i < 6; i++){
-        digitalWrite(MINUTES_PINS[i], MINUTES % 2);
-        Serial.print(MINUTES % 2);
-        MINUTES /= 2;
-    }
-    Serial.println();
-    Serial.println();
-  
-  delay(1000);
+void loop(){
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
+  Serial.println(WiFi.localIP());
+  sensorValue = analogRead(analogInPin);
+  Serial.println(sensorValue);
+  lcd.setCursor(0, 0);
+  lcd.print(sensorValue);
+  lcd.setCursor(8, 0);
+  lcd.print(timeClient.getFormattedTime());
+  lcd.setCursor(0,1);
+  lcd.print(WiFi.localIP());
+  delay(500);
 }
