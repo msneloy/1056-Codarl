e80438f92926a26ce8fac44bbde75600380ba2b3
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-03-18 03:57:52 +0800

    relay ops
    Executable segment sizes:
    IROM   : 277044          - code in flash         (default or ICACHE_FLASH_ATTR)
    IRAM   : 28128   / 32768 - code in IRAM          (ICACHE_RAM_ATTR, ISRs...)
    DATA   : 1296  )         - initialized variables (global, static) in RAM/HEAP
    RODATA : 2792  ) / 81920 - constants             (global, static) in RAM/HEAP
    BSS    : 25880 )         - zeroed variables      (global, static) in RAM/HEAP
    Sketch uses 309260 bytes (29%) of program storage space. Maximum is 1044464 bytes.
    Global variables use 29968 bytes (36%) of dynamic memory, leaving 51952 bytes for local variables. Maximum is 81920 bytes.
    esptool.py v2.8
    Serial port /dev/ttyUSB0
    Connecting....
    Chip is ESP8266EX
    Features: WiFi
    Crystal is 26MHz
    MAC: cc:50:e3:08:12:c5
    Uploading stub...
    Running stub...
    Stub running...
    Changing baud rate to 460800
    Changed.
    Configuring flash size...
    Auto-detected Flash size: 4MB
    Compressed 313408 bytes to 226861...
    Wrote 313408 bytes (226861 compressed) at 0x00000000 in 5.1 seconds (effective 491.6 kbit/s)...
    Hash of data verified.
    
    Leaving...
    Hard resetting via RTS pin...

--- 1056-Codarl.ino
+++ 1056-Codarl.ino
@@ -3,26 +3,103 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <TM1637Display.h>
+#include <ESPAsyncWebServer.h>
 #include "WAC.h"
+#include "GPIO.h"
 const char *ssid = SSID;
 const char *password = PW;
-#define CLK D4 
-#define DIO D3
-
-TM1637Display display = TM1637Display(CLK, DIO); // Create display object of type TM1637Display:
 //Create WAC.h to store creds
 //#define SSID "Wireless Access Point"
 //#define PW "Password"
+TM1637Display display = TM1637Display(CLK, DIO); // Create display object of type TM1637Display:
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
-
 int sensorValue = 0;  // value read from the pot
+
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 28800, 60000);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  
+const char* PARAM_INPUT_1 = "relay";  
+const char* PARAM_INPUT_2 = "state";
 
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    body {background: firebrick;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: red; border-radius: 34px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+    input:checked+.slider {background-color: green}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>Test Bench 01</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?relay="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?relay="+element.id+"&state=0", true); }
+  xhr.send();
+}</script>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    for(int i=1; i<=NUM_RELAYS; i++){
+      String relayStateValue = relayState(i);
+      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+    }
+    return buttons;
+  }
+  return String();
+}
+
+String relayState(int numRelay){
+  if(RELAY_NO){
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "";
+    }
+    else {
+      return "checked";
+    }
+  }
+  else {
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "checked";
+    }
+    else {
+      return "";
+    }
+  }
+  return "";
+}
 void setup(){
   Serial.begin(115200);
+  for(int i=1; i<=NUM_RELAYS; i++){
+    pinMode(relayGPIOs[i-1], OUTPUT);
+    if(RELAY_NO){
+      digitalWrite(relayGPIOs[i-1], HIGH);
+    }
+    else{
+      digitalWrite(relayGPIOs[i-1], LOW);
+    }
+  }
   display.clear();
   lcd.init();                   
   lcd.backlight();
@@ -39,6 +116,42 @@ void setup(){
     lcd.print(ssid); 
     Serial.print(".");
   }
+  Serial.println(WiFi.localIP());
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    String inputMessage2;
+    String inputParam2;
+    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      inputParam2 = PARAM_INPUT_2;
+      if(RELAY_NO){
+        Serial.print("NO ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
+      }
+      else{
+        Serial.print("NC ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage + inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
+  // Start server
+  server.begin();
   timeClient.begin();
   lcd.clear();
 }
---
+++ GPIO.h
@@ -0,0 +1,7 @@
+#define CLK D0 
+#define DIO D3
+#define RELAY_NO    true
+// Set number of relays
+#define NUM_RELAYS  5
+// Assign each GPIO to a relay
+int relayGPIOs[NUM_RELAYS] = {D5, D6, D7, D8, D4};
