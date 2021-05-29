26987e43d6a51059ac704d6604060f1b6ef0e4eb
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-05-30 00:36:54 +0800

    Update processor.h

--- processor.h
+++ processor.h
@@ -1,10 +1,10 @@
-String processor(const String& var){
+String processor(const String& var) {
   //Serial.println(var);
-  if(var == "BUTTONPLACEHOLDER"){
-    String buttons ="";
-    for(int i=1; i<=NUM_RELAYS; i++){
+  if (var == "BUTTONPLACEHOLDER") {
+    String buttons = "";
+    for (int i = 1; i <= NUM_RELAYS; i++) {
       String relayStateValue = relayState(i);
-      buttons+= "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i-1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+      buttons += "<h4>Relay #" + String(i) + " - GPIO " + relayGPIOs[i - 1] + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" " + relayStateValue + "><span class=\"slider\"></span></label>";
     }
     return buttons;
   }
