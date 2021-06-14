c228ceca039a9d97c34c02f2e78e39d068787132
Mahadi Sajjad Neloy <sierra117@qq.com>
2021-06-14 18:40:37 +0800

    Executable segment sizes: IROM   : 311868          - code in flash         (default or ICACHE_FLASH_ATTR)  IRAM   : 28020   / 32768 - code in IRAM          (ICACHE_RAM_ATTR, ISRs...)  DATA   : 1264  )         - initialized variables (global, static) in RAM/HEAP  RODATA : 4408  ) / 81920 - constants             (global, static) in RAM/HEAP  BSS    : 25936 )         - zeroed variables      (global, static) in RAM/HEAP
    esptool.py v2.8
    Serial port COM4
    Connecting....
    Chip is ESP8266EX
    Features: WiFi
    Crystal is 26MHz
    MAC: cc:50:e3:08:12:c5
    Uploading stub...
    Running stub...
    Stub running...
    Changing baud rate to 460800
    Changed.
    Configuring flash size...
    Auto-detected Flash size: 4MB
    Compressed 349712 bytes to 248208...
    Wrote 349712 bytes (248208 compressed) at 0x00000000 in 5.7 seconds (effective 491.7 kbit/s)...
    Hash of data verified.

--- 1056-Codarl.ino
+++ 1056-Codarl.ino
@@ -1,103 +1,68 @@
-#include <LiquidCrystal_I2C.h> //https://codeload.github.com/johnrickman/LiquidCrystal_I2C/zip/master
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+#include <LiquidCrystal_I2C.h> //https://codeload.github.com/johnrickman/LiquidCrystal_I2C/zip/master
 #include <TM1637Display.h>
-#include <ESPAsyncWebServer.h>
-#include "WAC.h"
-#include "GPIO.h"
-#include "WP.h"
-#include "relayState.h"
-#include "processor.h"
-const char *ssid = SSID;
-const char *password = PW;
-//Create WAC.h to store creds
-//#define SSID "Wireless Access Point"
-//#define PW "Password"
+WiFiServer server(80);
+String header;
+
+String output1State = "off";
+String output2State = "off";
+String output3State = "off";
+String output4State = "off";
+
+const int output1 = D3;
+const int output2 = D8;
+const int output3 = D7;
+const int output4 = D6;
+
 String newHostname = "1056 Codarl";
-TM1637Display display = TM1637Display(CLK, DIO); // Create display object of type TM1637Display:
+
+#define CLK D0
+#define DIO D5
+TM1637Display display = TM1637Display(CLK, DIO);
+
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 28800, 60000);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-const char* PARAM_INPUT_1 = "relay";
-const char* PARAM_INPUT_2 = "state";
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
 
 void setup() {
   Serial.begin(115200);
-  pinMode(D4, OUTPUT);
-  for (int i = 1; i <= NUM_RELAYS; i++) {
-    pinMode(relayGPIOs[i - 1], OUTPUT);
-    if (RELAY_NO) {
-      digitalWrite(relayGPIOs[i - 1], HIGH);
-    }
-    else {
-      digitalWrite(relayGPIOs[i - 1], LOW);
-    }
-  }
   display.clear();
   lcd.init();
   lcd.backlight();
   lcd.print("System Armed");
   lcd.clear();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-    //Get Current Hostname
+  Serial.println(WiFi.SSID());
+  //Get Current Hostname
   Serial.printf("Default hostname: %s\n", WiFi.hostname().c_str());
   //Set new hostname
   WiFi.hostname(newHostname.c_str());
   //Get Current Hostname
   Serial.printf("New hostname: %s\n", WiFi.hostname().c_str());
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    lcd.setCursor(0, 0);
-    lcd.print("Connecting to");
-    lcd.setCursor(0, 1);
-    lcd.print(ssid);
-    Serial.print(".");
-  }
-  Serial.println(WiFi.localIP());
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send_P(200, "text/html", index_html, processor);
-  });
 
-  // Send a GET request to <ESP_IP>/update?relay=<inputMessage>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest * request) {
-    String inputMessage;
-    String inputParam;
-    String inputMessage2;
-    String inputParam2;
-    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
-    if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
-      inputMessage = request->getParam(PARAM_INPUT_1)->value();
-      inputParam = PARAM_INPUT_1;
-      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
-      inputParam2 = PARAM_INPUT_2;
-      if (RELAY_NO) {
-        Serial.print("NO ");
-        digitalWrite(relayGPIOs[inputMessage.toInt() - 1], !inputMessage2.toInt());
-      }
-      else {
-        Serial.print("NC ");
-        digitalWrite(relayGPIOs[inputMessage.toInt() - 1], inputMessage2.toInt());
-      }
-    }
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
-    }
-    Serial.println(inputMessage + inputMessage2);
-    request->send(200, "text/plain", "OK");
-  });
-  // Start server
+  pinMode(D4, OUTPUT);
+
+  pinMode(output1, OUTPUT);
+  pinMode(output2, OUTPUT);
+  pinMode(output3, OUTPUT);
+  pinMode(output4, OUTPUT);
+
+  digitalWrite(output1, LOW);
+  digitalWrite(output2, LOW);
+  digitalWrite(output3, LOW);
+  digitalWrite(output4, LOW);
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("AutoConnectAP");
+  Serial.println("Connected.");
   server.begin();
-  timeClient.begin();
-  lcd.clear();
 }
 
 void loop() {
@@ -106,10 +71,9 @@ void loop() {
   Serial.println(timeClient.getFormattedTime());
   Serial.println(WiFi.localIP());
   lcd.setCursor(0, 0);
-  lcd.print(ssid);
+  lcd.print(WiFi.SSID());
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP());
-  delay(500);
   int A, B;
   display.setBrightness(7); // Set the brightness:
   A = timeClient.getHours() * 100 + timeClient.getMinutes();
@@ -121,6 +85,115 @@ void loop() {
   else
   {
     display.showNumberDecEx(A, 0b00000000, false, 4, 0);
+  }
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        header += c;
+        if (c == '\n') {
+
+          if (currentLine.length() == 0) {
+
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            if (header.indexOf("GET /1/on") >= 0) {
+              Serial.println("output1State on");
+              output1State = "on";
+              digitalWrite(output1, HIGH);
+            }
+            else if (header.indexOf("GET /1/off") >= 0) {
+              Serial.println("output1State off");
+              output1State = "off";
+              digitalWrite(output1, LOW);
+            }
+
+            else if (header.indexOf("GET /2/on") >= 0) {
+              Serial.println("output2State on");
+              output2State = "on";
+              digitalWrite(output2, HIGH);
+            }
+            else if (header.indexOf("GET /2/off") >= 0) {
+              Serial.println("output2State off");
+              output2State = "off";
+              digitalWrite(output2, LOW);
+            }
+
+            else if (header.indexOf("GET /3/on") >= 0) {
+              Serial.println("output3State on");
+              output3State = "on";
+              digitalWrite(output3, HIGH);
+            } else if (header.indexOf("GET /3/off") >= 0) {
+              Serial.println("output3State off");
+              output3State = "off";
+              digitalWrite(output3, LOW);
+            }
+
+            else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("output4State on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            }
+            else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("output4State off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: green; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: red;}</style></head>");
+            client.println("<body><h1>Prototype 420.69</h1>");
+            client.println("<p>One : " + output1State + "</p>");
+            if (output1State == "off") {
+              client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("<p>Two : " + output2State + "</p>");
+            if (output2State == "off") {
+              client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("<p>Three : " + output3State + "</p>");
+            if (output3State == "off") {
+              client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("<p>Four : " + output4State + "</p>");
+            if (output4State == "off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
   digitalWrite(D4, LOW);
 }
--- GPIO.h
+++
@@ -1,7 +0,0 @@
-#define CLK D0
-#define DIO D5
-#define RELAY_NO    true
-// Set number of relays
-#define NUM_RELAYS  4
-// Assign each GPIO to a relay
-int relayGPIOs[NUM_RELAYS] = {D3, D8, D7, D6};
